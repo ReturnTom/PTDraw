00e47d2bd626edbf4b42dfb29775792c0227bcf9
pt <ios-hsh@sammacmini.local>
2019-01-08 13:44:25 +0800

    add project

---
+++ PTDraw/PTDraw/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PTDraw
+//
+//  Created by pt on 2019/1/8.
+//  Copyright © 2019年 pt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PTDraw/PTDraw/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PTDraw
+//
+//  Created by pt on 2019/1/8.
+//  Copyright © 2019年 pt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
